702756e1dbccfb8f3c45c7740e31216070ea6202
devdeepakkumar25 <devdeepakkumar25@gmail.com>
2025-11-17 03:02:55 +0530

    BuildingComponent file created

---
+++ buildingComponent/src/App.tsx
@@ -0,0 +1,12 @@
+import Message from "./Message"
+
+const App = () => {
+  return (
+    <div>
+
+      <Message />
+    </div>
+  )
+}
+
+export default App
---
+++ buildingComponent/src/Message.tsx
@@ -0,0 +1,11 @@
+
+const Message = () => {
+  const name = "Deepak"
+  if (name)
+    return (
+      <h1>Hello {name}</h1>
+    )
+  return <h1> Hello World</h1>
+}
+
+export default Message
